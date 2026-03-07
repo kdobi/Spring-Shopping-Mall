464052cbb4a81be0d7680032e27ea88ea8dbeb4c
김동빈 <kdobi87@naver.com>
2026-03-07 20:58:55 +0900

    first commit

---
+++ src/main/java/Domain/Spring_Shopping_Mall/SpringShoppingMallApplication.java
@@ -0,0 +1,13 @@
+package Domain.Spring_Shopping_Mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShoppingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShoppingMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Domain/Spring_Shopping_Mall/SpringShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package Domain.Spring_Shopping_Mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShoppingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
